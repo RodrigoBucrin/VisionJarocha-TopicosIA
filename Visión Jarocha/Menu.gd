41879fa357be43c093a5720698b22f3a359eb7d5
RodrigoBucrin <42250801+RodrigoBucrin@users.noreply.github.com>
2021-01-15 09:40:28 -0600

    Add files via upload

---
+++ Visión Jarocha/Menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export (PackedScene) var scn_name
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+		 # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene_to(scn_name)
+#	pass
